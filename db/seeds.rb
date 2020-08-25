f9b17bf120de6432b8c8147d9b7dd287d2e96873
Wince Shum <wince.shum@gmail.com>
2020-08-25 18:17:44 -0400

    made half of a seed and custom validation in user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'seed started'
+
+Rating.destroy_all
+UserAction.destroy_all
+Action.destroy_all
+User.destroy_all
+
+kai = User.new(
+  email: 'kai@email.com',
+  password: '123456',
+  password_confirmation: '123456',
+  address: "5333 Casgrain Avenue",
+  availability: ['Morning', 'Afternoon']
+)
+
+kai.save
+
+axe = Action.new(
+  name: 'axe throwing',
+  type: 'Activity',
+  happy_sad: 4,
+  calm_angry: 10,
+  highenergy_lowenergy: 0,
+  social_anxious: 5,
+  duration: ,
+  time_of_day: ['Morning', 'Afternoon', 'Evening'] )
+
+puts 'seed ended'
