c9fcca5e6711ba8307c0ba30018437c0a24677f8
Daniel Lee <ephplease@gmail.com>
2020-08-25 17:02:04 -0400

    Create basic view and controller for mood

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,7 +1,28 @@
 class MoodsController < ApplicationController
   def new
+    @mood = Mood.new
+    authorize(@mood)
   end
 
   def create
+    @mood = Mood.new(mood_params)
+    puts mood_params
+    @mood.user = current_user
+    authorize(@mood)
+    if @mood.save
+      if params[:type] == "activity"
+        redirect_to activities_path
+      elsif params[:type] == "medium"
+        redirect_to mediums_path
+      end
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def mood_params
+    params.require(:mood).permit(:happy_sad, :calm_angry, :highenergy_lowenergy, :social_anxious)
   end
 end
