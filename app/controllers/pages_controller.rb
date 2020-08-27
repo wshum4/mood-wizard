bc73631f97aeb9f59c10b66e4115e329736f0d18
Wince Shum <wince.shum@gmail.com>
2020-08-27 12:27:26 -0400

    rechecked all route and view pages to work"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,14 +6,16 @@ class PagesController < ApplicationController
 
   def dashboard
     # Redirects the user to login page if they are not logged in
-    # redirect_to new_user_session_path if current_user.nil?
+    redirect_to new_user_session_path if current_user.nil?
+    # redirect_to new_mood_path if current_user.last_mood.nil?
     # # Redirects the user to enter a mood if they haven't in 1 hour (in seconds)
-    # redirect_to new_mood_path if current_user.last_mood.time_since > 3600
+    redirect_to new_mood_path if current_user.moods.length > 0 && current_user.last_mood.time_since > 3600
     # Redirects user to graph display
     # Redirects user to past activities
-    @past_activities = Rating.where(params[:user_id])
+    @current_user_id = current_user.id
+    @past_mediums = UserAction.joins(:action).where(user_id: @current_user_id, actions: { action_type: 'Medium' })
+    @past_activities = Rating.joins(:action).where(user_id: @current_user_id)
     # Redirect user to past medium
-    @past_mediums = Action.where({ user_id: current_user.id, action_type: 'Medium' })
     # @past_medium = current_user.actions.filter do |action|
     #   action.action_type == 'Medium'
     # end
