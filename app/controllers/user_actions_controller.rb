6fa4dfe290ffdb52ebe237bb839571983e64d4d9
Wince Shum <61887264+wshum4@users.noreply.github.com>
2020-09-02 10:18:03 -0400

    Update user_actions_controller.rb

--- app/controllers/user_actions_controller.rb
+++ app/controllers/user_actions_controller.rb
@@ -7,9 +7,6 @@ class UserActionsController < ApplicationController
     if @user_action.save
       flash[:booking] = { action_id: @user_action.action_id }
       redirect_to mediums_path(action_id: "#{@user_action.action_id}")
-      # else
-      #   redirect_to('https://mood-wizard.herokuapp.com/mediums', action_id: "#{@user_action.action_id}")
-      # end
     else
       flash[:alert] = "Something went wrong with booking!"
       redirect_to activity_path(params[:action_id])
